a8c9bab9431c093db66ce67ce24584c42c5e7393
Alhassan Sahad <98456443+asahad@users.noreply.github.com>
2023-11-01 11:41:51 -0500

    Initialize react project using vite bundler

---
+++ TrackMyHunt/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <h1>
+      The Process of Identifying what every portion of a land is suitable for
+      locating facilities based on the layout plan of the area is what we are
+      terming as zoning
+    </h1>
+  );
+}
+export default App;
