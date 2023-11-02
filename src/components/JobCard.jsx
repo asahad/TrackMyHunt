8ed9ecee1eacd37cfe5fd7a253a8509fc936e757
Alhassan Sahad <98456443+asahad@users.noreply.github.com>
2023-11-01 22:41:58 -0500

    added trash icon from react-icons for deleting jobcards

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,31 +1,43 @@
 // JobCard.jsx
-import { Card } from 'react-bootstrap';
-import { useDrag } from 'react-dnd';
-import { ItemTypes } from './Constants';
+import { Button, Card, Row, Col } from "react-bootstrap";
+import { useDrag } from "react-dnd";
+import { ItemTypes } from "./Constants";
+import { BiTrash } from "react-icons/bi";
 
-const JobCard=({ job })=> {
+const JobCard = ({ job }) => {
   const [{ opacity }, dragRef] = useDrag(
     () => ({
       type: ItemTypes.CARD,
       item: { job },
       collect: (monitor) => ({
-        opacity: monitor.isDragging() ? 0.5 : 1
-      })
+        opacity: monitor.isDragging() ? 0.5 : 1,
+      }),
     }),
     [job]
   );
   return (
-    <Card 
-      ref={dragRef} 
-      style={{ opacity }}
-      className="job-card mb-3"
-    >
-      <Card.Body>
-        <Card.Title>{job.company}</Card.Title>
-        <Card.Text>{job.position}</Card.Text>
-        <Card.Text>{job.daysAgo} days ago</Card.Text>
-      </Card.Body>
-    </Card>
+    <>
+      <Card ref={dragRef} style={{ opacity }} className="job-card mb-3">
+        <Card.Body>
+          <Row>
+            <Col xs={6}>
+              <Card.Title>{job.company}</Card.Title>
+              <Card.Text>{job.position}</Card.Text>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6}>
+              <Card.Text>{job.daysAgo} days ago</Card.Text>
+            </Col>
+            <Col xs={6}>
+              <Card.Text>
+                <BiTrash />
+              </Card.Text>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </>
   );
-}
+};
 export default JobCard;
